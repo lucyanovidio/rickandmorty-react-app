3c033715a1aadbd2a35e0bcede2c47bbe439d4c7
lucyanovidio <lucyanovidio@gmail.com>
2023-08-14 21:40:12 -0300

    :bug: fix: Corrigindo bug da paginação

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
   async function getData(page) {
     setInfo(await getChars(page));
   }
-  
+
   useEffect(() => {
     getData(counter);
   }, []);
@@ -58,73 +58,78 @@ function Home() {
     getData(Number(e.target.textContent));
   }
 
-  console.log(counter)
+  console.log(counter);
 
   return (
     <div className="container">
       <h1>Rick and Morty App</h1>
 
-        {Object.keys(info).length > 0 ? (
-          <ul className="chars-container">
-            {info.results.map((char) => (
-              <li
-                className="char"
-                key={char.id}
-                id={"-" + char.id}
-                onClick={showCharPage}
-                title="Ver personagem"
-              >
-                <img
-                  src={char.image}
-                  alt={`Imagem do personagem ${char.name}.`}
-                />
-                <p>{char.name}</p>
-              </li>
-            ))}
-          </ul>
-        ) : (<p className="text-2">Carregando...</p>)}
+      {Object.keys(info).length > 0 ? (
+        <ul className="chars-container">
+          {info.results.map((char) => (
+            <li
+              className="char"
+              key={char.id}
+              id={"-" + char.id}
+              onClick={showCharPage}
+              title="Ver personagem"
+            >
+              <img
+                src={char.image}
+                alt={`Imagem do personagem ${char.name}.`}
+              />
+              <p>{char.name}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-2">Carregando...</p>
+      )}
 
       <div className="buttonsContainer">
-        
         <button onClick={handlePrevious}>
           <BsArrowLeftCircle />
         </button>
 
         <div className="pagesNums">
-          {Array.from(Array(pagesAmount), (_, index) => (
-            <button 
-              key={counter + index}
-              className={
-                // Mantém estilo para página 1
-                counter === 1
-                  ? "current"
-                  : ""
-              }
-              onClick={goToPage}
-            >{
-              (counter >= totalPages - 2) 
-                ? (totalPages - pagesAmount + 1 + index) // chegar no primeiro da lista de botões pra depois somar os index e ficar certo
-                : (counter <= 3 ? index + 1 : counter + index - 2)
-            }</button>
-          ))}
+          {Object.keys(info).length > 0
+            ? Array.from(Array(pagesAmount), (_, index) => (
+                <button
+                  key={counter + index}
+                  className={
+                    // Mantém estilo para página 1
+                    counter === 1 ? "current" : ""
+                  }
+                  onClick={goToPage}
+                >
+                  {counter >= totalPages - 2
+                    ? totalPages - pagesAmount + 1 + index // chegar no primeiro da lista de botões pra depois somar os index e ficar certo
+                    : counter <= 3
+                    ? index + 1
+                    : counter + index - 2}
+                </button>
+              ))
+            : "..."}
         </div>
 
         {/* Aplicar estilo a página atual */}
 
-        {Array.from(document.querySelectorAll(".pagesNums button"), (button, index) => {
-          setTimeout(() => {
-            if (counter == button.textContent) {
-              button.classList.add("current");
-            } else {
-              button.classList.remove("current");
-            }
-          }, 1);
-        })}
-        
+        {Array.from(
+          document.querySelectorAll(".pagesNums button"),
+          (button, index) => {
+            setTimeout(() => {
+              if (counter == button.textContent) {
+                button.classList.add("current");
+              } else {
+                button.classList.remove("current");
+              }
+            }, 1);
+          }
+        )}
+
         <button onClick={handleNext}>
           <BsArrowRightCircle />
         </button>
-
       </div>
     </div>
   );
